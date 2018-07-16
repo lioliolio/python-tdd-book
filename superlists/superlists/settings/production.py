bd60d94e1bbcfdb3a2453c200aee1f9bfcc56594
lioliolio <kpc00100@gmail.com>
2018-07-16 15:40:30 +0900

    장고 프로젝트 초기화했습니다
    장고 프로젝트 초기화 하면서 변경한 내용입니다.
    - DATABASE, DJANGO_SECRET_KEY를 환경변수로 분리
    - Django 설정 파일 develop, production으로 분리

---
+++ superlists/superlists/settings/production.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': get_env_variable('DATABASE_HOST'),
+        'PORT': get_env_variable('DATABASE_PORT'),
+    },
+}
